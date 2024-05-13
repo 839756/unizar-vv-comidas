452347a955707b9f8c25f17f04fd8fc59457c8d0
Andrei Vlasceanu <839756@unizar.es>
2024-05-13 15:23:01 +0200

    Funciones pruebas de caminos 🛤

--- app/src/androidTest/java/es/unizar/eina/T223_comidas/espresso/EspressoTest.java
+++ app/src/androidTest/java/es/unizar/eina/T223_comidas/espresso/EspressoTest.java
@@ -1,20 +1,18 @@
 package es.unizar.eina.T223_comidas.espresso;
 
-
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.hamcrest.Matchers.allOf;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -36,10 +34,12 @@ public class EspressoTest {
     private int numeroPedidosInicial;
     private int numeroPedidos;
 
-    private String NOMBRE_PLATOS = "Test plato";
-    private double PRECIO_PLATO = 1.0;
-    private String SUFIJO_NOMRE_PLATO = " - " + PRECIO_PLATO + " €";
-    private String NOMBRE_PEDIDO = "Test pedido";
+    private final String NOMBRE_PLATOS = "Test plato";
+    private final double PRECIO_PLATO = 1.0;
+    private final String SUFIJO_PLATO = " - " + PRECIO_PLATO + " €";
+    private final String NOMBRE_PEDIDO = "Test pedido";
+    private final String FECHA_PEDIDO = "23/05/2024/20:00";
+    private final String SUFIJO_PEDIDO = " - " + FECHA_PEDIDO;
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityRule
@@ -47,13 +47,10 @@ public class EspressoTest {
 
     @Test
     public void test() {
-        onView(withText("PLATOS")).perform(click());
+        onView(withText("PEDIDOS")).perform(click());
 
-        String nombrePlato = "Helado" + SUFIJO_NOMRE_PLATO;
-        // Scroll to the RecyclerView item with the specified text
-        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
-        onView(withText(nombrePlato)).perform(longClick());
-        onView(withText("Eliminar Plato")).perform(click());
+        caminoCrearPedido();
+        caminoConfirmarCrearPedido();
     }
 
     private void ejecutarCamino(String[] camino) {
@@ -69,11 +66,54 @@ public class EspressoTest {
                     caminoOrdenarPedido();
                     break;
                 case "4":
+                    caminoCrearPedido();
                     break;
                 case "5":
                     caminoEliminarPedido();
                     break;
-
+                case "6":
+                    caminoModificarPedido();
+                    break;
+                case "7":
+                case "12":
+                    caminoVolverHome();
+                    break;
+                case "8":
+                    caminoNoConfirmarCrearPedido();
+                    break;
+                case "9":
+                    caminoConfirmarCrearPedido();
+                    break;
+                case "10":
+                    caminoNoConfirmarModificarPedido();
+                    break;
+                case "11":
+                    caminoConfirmarModificarPedido();
+                    break;
+                case "13":
+                    caminoCrearPlato();
+                    break;
+                case "14":
+                    caminoEliminarPlato();
+                    break;
+                case "15":
+                    caminoOrdenarPlato();
+                    break;
+                case "16":
+                    caminoModificarPlato();
+                    break;
+                case "17":
+                    caminoNoConfirmarCrearPlato();
+                    break;
+                case "18":
+                    caminoConfirmarCrearPlato();
+                    break;
+                case "19":
+                    caminoNoConfirmarModificarPlato();
+                    break;
+                case "20":
+                    caminoConfirmarModificarPlato();
+                    break;
                 default:
                     return;
             }
@@ -93,18 +133,143 @@ public class EspressoTest {
     }
 
     private void caminoEliminarPedido() {
+        String nombrePedido = this.NOMBRE_PEDIDO + " " + (this.numeroPedidos - 1) + this.SUFIJO_PEDIDO;
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePedido))));
+        onView(withText(nombrePedido)).perform(longClick());
+
+        if (this.numeroPedidos < this.numeroPedidosInicial) {
+            onView(withText("Eliminar Pedido")).perform(click());
+            this.numeroPedidos--;
+        } else {
+            pressBack();
+        }
+    }
+
+    private void caminoCrearPedido() {
+        onView(withText("CREAR")).perform(click());
+
+        String nombrePedido = this.NOMBRE_PEDIDO + " " + this.numeroPedidos;
+
+        onView(withId(R.id.etCustomerName)).perform(replaceText(nombrePedido), closeSoftKeyboard());
+
+        onView(withId(R.id.etPhoneNumber)).perform(replaceText("612345678"), closeSoftKeyboard());
+
+        String[] parts = FECHA_PEDIDO.split("/");
+        String formattedDate = parts[0] + "/" + parts[1] + "/" + parts[2];
+        String formattedTime = parts[3];
+
+        onView(withId(R.id.etPickupDate)).perform(replaceText(formattedDate + "/" + formattedTime), closeSoftKeyboard());
+    }
+
+    private void caminoNoConfirmarCrearPedido() {
+        pressBack();
+    }
+
+    private void caminoConfirmarCrearPedido() {
+        this.numeroPedidos++;
+        onView(withId(R.id.btnConfirmOrder)).perform(click());
+        onView((withText("No enviar info"))).perform(click());
+
+        String nombrePedido = this.NOMBRE_PEDIDO + " " + (this.numeroPedidos - 1) + this.SUFIJO_PEDIDO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePedido))));
+        onView(withText(nombrePedido)).check(matches(isDisplayed()));
+    }
+
+    private void caminoModificarPedido() {
+        String nombrePedido = this.NOMBRE_PEDIDO + " " + (this.numeroPedidos - 1) + this.SUFIJO_PEDIDO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePedido))));
+        onView(withText(nombrePedido)).perform(click());
+
+        String nuevoNombrePedido = this.NOMBRE_PEDIDO + " " + (this.numeroPedidos - 1);
+
+        onView(withId(R.id.etCustomerName)).perform(replaceText(nuevoNombrePedido), closeSoftKeyboard());
+    }
+
+    private void caminoNoConfirmarModificarPedido() {
+        pressBack();
+    }
+    private void caminoConfirmarModificarPedido() {
+        onView(withId(R.id.btnConfirmOrder)).perform(click());
+        onView((withText("No enviar info"))).perform(click());
+
+        String nombrePedido = this.NOMBRE_PEDIDO + " " + (this.numeroPedidos - 1) + this.SUFIJO_PEDIDO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePedido))));
+        onView(withText(nombrePedido)).check(matches(isDisplayed()));
+    }
+
+    private void caminoVolverHome() {
+        onView(withId(R.id.botonInicio1)).perform(click());
+    }
+
+    private void caminoEliminarPlato() {
+        String nombrePlato = this.NOMBRE_PLATOS + " " + (this.numeroPlatos - 1) + this.SUFIJO_PLATO;
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
+        onView(withText(nombrePlato)).perform(longClick());
 
         if (this.numeroPlatos < this.numeroPlatosInicial) {
-            String nombrePlato = this.NOMBRE_PLATOS + " " + this.numeroPlatos + this.SUFIJO_NOMRE_PLATO;
-            onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
-            onView(withText(nombrePlato)).perform(longClick());
             onView(withText("Eliminar Plato")).perform(click());
-
             this.numeroPlatos--;
+        } else {
+            pressBack();
         }
+    }
 
+    private void caminoOrdenarPlato() {
+        onView(withText("ORDENAR")).perform(click());
+        onView(withText("ORDENAR POR NOMBRE")).perform(click());
+    }
+
+    private void caminoCrearPlato() {
+        onView(withText("CREAR")).perform(click());
+
+        String nombrePlato = this.NOMBRE_PLATOS + " " + this.numeroPlatos;
+
+        onView(withId(R.id.nombrePlato)).perform(replaceText(nombrePlato), closeSoftKeyboard());
+
+        onView(withId(R.id.precioPlato)).perform(replaceText("33"), closeSoftKeyboard());
     }
 
 
+
+    private void caminoNoConfirmarCrearPlato() {
+        pressBack();
+    }
+
+    private void caminoConfirmarCrearPlato() {
+        this.numeroPlatos++;
+        onView(withId(R.id.button_save)).perform(click());
+
+        String nombrePlato = this.NOMBRE_PLATOS + " " + (this.numeroPlatos - 1) + this.SUFIJO_PLATO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
+        onView(withText(nombrePlato)).check(matches(isDisplayed()));
+    }
+
+    private void caminoModificarPlato() {
+        String nombrePlato = this.NOMBRE_PLATOS + " " + (this.numeroPlatos - 1) + this.SUFIJO_PLATO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
+        onView(withText(nombrePlato)).perform(click());
+
+        String descripcion = this.NOMBRE_PLATOS + " " + (this.numeroPlatos - 1);
+
+        onView(withId(R.id.descPlato)).perform(replaceText(descripcion), closeSoftKeyboard());
+    }
+
+    private void caminoConfirmarModificarPlato() {
+        onView(withId(R.id.button_save)).perform(click());
+
+        String nombrePlato = this.NOMBRE_PLATOS + " " + (this.numeroPlatos - 1) + this.SUFIJO_PLATO;
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText(nombrePlato))));
+        onView(withText(nombrePlato)).check(matches(isDisplayed()));
+    }
+
+    private void caminoNoConfirmarModificarPlato() {
+        pressBack();
+    }
 
 }
