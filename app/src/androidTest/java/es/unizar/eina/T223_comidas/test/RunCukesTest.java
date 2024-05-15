9573af0e861f3225980010008b3ce73a354fc6cc
839756 <839756@unizar.es>
2024-05-15 17:29:45 +0200

    Funciona cucumber

--- app/src/androidTest/java/es/unizar/eina/T223_comidas/test/RunCukesTest.java
+++ app/src/androidTest/java/es/unizar/eina/T223_comidas/test/RunCukesTest.java
@@ -3,7 +3,7 @@ package es.unizar.eina.T223_comidas.test;
 import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(
-        glue = { "es.unizar.eina.T223_comidas.test" }, tags = "~@wip" , features = { "features" }
+        /*glue = { "es.unizar.eina.T223_comidas.test" }, tags = "~@wip" , */features = { "features" }
 )
 
 public class RunCukesTest {
