5fae9da4de673394c0c01ee112f5cb5fd036671a
Miguel Ángel Latre Abadía <latre@unizar.es>
2024-05-15 11:24:56 +0200

    Configuración Cucumber

--- app/src/androidTest/java/es/unizar/eina/T223_comidas/test/RunStepsDefinition.java
+++ app/src/androidTest/java/es/unizar/eina/T223_comidas/test/RunStepsDefinition.java
@@ -17,11 +17,11 @@ import static org.junit.Assert.assertNotNull;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import es.unizar.eina.T223_comidas.R;
 
